5c01f9a0c8ff016e07f56040093a1f53c4574566
Rodrigo Ramirez <rhoproduccionesbr@gmail.com>
2026-03-07 15:00:04 -0300

    Organizando imagenes de zocalos en subcarpeta e implementando datos dinamicos JS

---
+++ imagenes-data.js
@@ -0,0 +1,25 @@
+// =====================================================================
+//  LISTA DE IMÁGENES DE ZÓCALOS
+//  Para agregar una imagen, colocala en la carpeta "zocalos" y agregá su nombre aquí.
+// =====================================================================
+const ZOCALOS_DATA = [
+    "zocalos/El profe.png",
+    "zocalos/Manuel  enciso.png",
+    "zocalos/abog carlos centurion.png",
+    "zocalos/amadito.png",
+    "zocalos/bodega packo.png",
+    "zocalos/clinica  san  francisco.png",
+    "zocalos/hector ramos2.png",
+    "zocalos/hernan martinez.png",
+    "zocalos/herreria  sandoval.png",
+    "zocalos/imprecor.png",
+    "zocalos/julio gamarra.png",
+    "zocalos/laura acosta.png",
+    "zocalos/libreria gonza.png",
+    "zocalos/reybeinitez.png",
+    "zocalos/ricardo  arroyo.png",
+    "zocalos/rolo  nuñez.png",
+    "zocalos/terere el gordo.png",
+    "zocalos/zonia  boutique.png"
+    // ← Agregar acá el nombre de nuevas imágenes respetando el formato anterior
+];
