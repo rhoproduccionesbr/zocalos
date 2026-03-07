d97af75081964bb91ea4deb325e6771b40e39848
Rodrigo Ramirez <rhoproduccionesbr@gmail.com>
2026-03-07 15:16:27 -0300

    Auto-generar imagenes-data.js reparando el error de sintaxis

--- imagenes-data.js
+++ imagenes-data.js
@@ -1,26 +1,25 @@
-// =====================================================================
-//  LISTA DE IMÁGENES DE ZÓCALOS
-//  Para agregar una imagen, colocala en la carpeta "zocalos" y agregá su nombre aquí.
-// =====================================================================
-const ZOCALOS_DATA = [
-    "zocalos/El profe.png",
-    "zocalos/Manuel  enciso.png",
-    "zocalos/abog carlos centurion.png",
-    "zocalos/amadito.png",
-    "zocalos/bodega packo.png",
-    "zocalos/clinica  san  francisco.png",
-    "zocalos/hector ramos2.png",
-    "zocalos/hernan martinez.png",
-    "zocalos/herreria  sandoval.png",
-    "zocalos/imprecor.png",
-    "zocalos/julio gamarra.png",
-    "zocalos/laura acosta.png",
-    "zocalos/libreria gonza.png",
-    "zocalos/reybeinitez.png",
-    "zocalos/ricardo  arroyo.png",
-    "zocalos/rolo  nuñez.png",
-    "zocalos/terere el gordo.png",
-    "zocalos/zonia  boutique.png"
-    "zocalos/gringos.png"
-    // ← Agregar acá el nombre de nuevas imágenes respetando el formato anterior
-];
+// ===================================================================== 
+//  LISTA DE IMAGENES DE ZOCALOS 
+//  Generado automaticamente 
+// ===================================================================== 
+const ZOCALOS_DATA = [ 
+    "zocalos/abog carlos centurion.png", 
+    "zocalos/amadito.png", 
+    "zocalos/bodega packo.png", 
+    "zocalos/clinica  san  francisco.png", 
+    "zocalos/El profe.png", 
+    "zocalos/gringos.png", 
+    "zocalos/hector ramos2.png", 
+    "zocalos/hernan martinez.png", 
+    "zocalos/herreria  sandoval.png", 
+    "zocalos/imprecor.png", 
+    "zocalos/julio gamarra.png", 
+    "zocalos/laura acosta.png", 
+    "zocalos/libreria gonza.png", 
+    "zocalos/Manuel  enciso.png", 
+    "zocalos/reybeinitez.png", 
+    "zocalos/ricardo  arroyo.png", 
+    "zocalos/rolo  nuñez.png", 
+    "zocalos/terere el gordo.png", 
+    "zocalos/zonia  boutique.png", 
+]; 
